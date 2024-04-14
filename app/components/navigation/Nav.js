b8a86e69a02a12831b43515d1e6b3b745f919524
Matt Miller <mattmillerf@pm.me>
2024-04-14 00:12:27 -0400

    add icons to nav

--- app/components/navigation/Nav.js
+++ app/components/navigation/Nav.js
@@ -3,17 +3,40 @@ import Image from "next/image";
 import SignIn from "./Login";
 import SignOut from "./Logout";
 import { auth } from "@/auth"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
+import { faScrewdriverWrench, faMap, faHome } from "@fortawesome/free-solid-svg-icons";
 
 export default async function Nav() {
     const session = await auth()
 
     return (
         <nav className="main-nav">
-            <Link href="/">Home</Link>
-            <Link href="/tools">Tools</Link>
+            <Link href="/">
+                <FontAwesomeIcon
+                    className="user-control"
+                    title={"Home"}
+                    style={{fontSize:"25px"}}
+                    icon={faHome}
+                ></FontAwesomeIcon>
+            </Link>
+            <Link href="/tools">
+                <FontAwesomeIcon
+                    className="user-control"
+                    title={"Tools/Calculators"}
+                    style={{fontSize:"25px"}}
+                    icon={faScrewdriverWrench}
+                ></FontAwesomeIcon>
+            </Link>
             {session?.user ? (
                 <>
-                    <Link href="/map">Map</Link>
+                    <Link href="/map">
+                        <FontAwesomeIcon
+                            className="user-control"
+                            title={"Map"}
+                            style={{fontSize:"25px"}}
+                            icon={faMap}
+                        ></FontAwesomeIcon>
+                    </Link>
                     <Image className="user_profile"
                         src={session?.user.image}
                         alt="user profile"
