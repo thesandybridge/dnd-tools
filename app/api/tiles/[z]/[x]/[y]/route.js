6d1ce2be2d0797535f3f3e4f2c052f3ba6c14fea
Matt Miller <mattmillerf@pm.me>
2024-04-15 09:54:10 -0400

    updates

--- app/api/tiles/[z]/[x]/[y]/route.js
+++ app/api/tiles/[z]/[x]/[y]/route.js
@@ -10,18 +10,16 @@ const s3Client = new S3Client({
 
 export async function GET(request, {params}) {
     const { z, x, y } = params;
-    const fileName = `${y}`;  // Assuming that `y` includes the filename without extension
+    const fileName = `${y}`;
 
-    // Define the command to get an object
     const command = new GetObjectCommand({
-        Bucket: 'dndeberron',  // Your S3 Bucket
-        Key: `eberron/${z}/${x}/${fileName}`  // Construct the S3 object key
+        Bucket: 'dndeberron',
+        Key: `eberron/${z}/${x}/${fileName}`
     });
 
     try {
         const { Body } = await s3Client.send(command);
 
-        // Create a response and set the content type based on your file type, assumed to be 'image/png'
         return new Response(Body, {
             status: 200,
             headers: {
@@ -32,7 +30,6 @@ export async function GET(request, {params}) {
         console.error('Error fetching tile:', error);
         console.log(`Z: ${z}, X: ${x}, Y: ${y}`);
 
-        // Return a JSON response with an error message if fetching fails
         return new Response(JSON.stringify({ error: 'Failed to fetch tile', details: error.message }), {
             status: 500,
             headers: {
