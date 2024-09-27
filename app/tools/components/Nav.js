292246713efaa2e7c446b3ee80072f7b4c1073c0
Matt Miller <mattmillerf@pm.me>
2024-09-27 14:03:26 -0400

    add hover

--- app/tools/components/Nav.js
+++ app/tools/components/Nav.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBackward } from "@fortawesome/free-solid-svg-icons"
 export default function Nav () {
   return (
-    <Link style={{alignSelf: 'flex-start'}} href="/tools">
+    <Link className="back" style={{alignSelf: 'flex-start'}} href="/tools">
       <FontAwesomeIcon
         icon={faBackward}
       /> Back
