863e98a6aea167c1cfe313caf13cd674d8eb266b
Matt Miller <mattmillerf@pm.me>
2024-09-27 16:27:58 -0400

    remove deprecated domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['www.dndbeyond.com', 'cdn.discordapp.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.dndbeyond.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+    ],
   },
 };
 
