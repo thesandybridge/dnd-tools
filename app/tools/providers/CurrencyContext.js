9322e15917bc73c02ec34d24d2801e00e5258886
Matt Miller <mattmillerf@pm.me>
2024-09-27 20:47:57 -0400

    rename

--- app/tools/providers/CurrencyContext.js
+++ app/tools/providers/CurrencyContext.js
@@ -1,19 +1,19 @@
 'use client'
 
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext } from "react"
 
-const CurrencyContext = createContext();
+const CurrencyContext = createContext()
 
 export const CurrencyProvider = ({ children }) => {
-    const [currency, setCurrency] = useState(0);
+    const [currency, setCurrency] = useState(0)
 
     return (
         <CurrencyContext.Provider value={{ currency, setCurrency }}>
             {children}
         </CurrencyContext.Provider>
-    );
-};
+    )
+}
 
 export const useCurrency = () => {
-    return useContext(CurrencyContext);
-};
+    return useContext(CurrencyContext)
+}
